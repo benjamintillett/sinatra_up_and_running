6a1240eb3dfb5d3fd4af0d44a6e07f372c735382
benjamintillett <benjamin.tillett@gmail.com>
2014-10-12 20:43:58 +0100

    finish sessions start cookies

--- hello.rb
+++ hello.rb
@@ -1,10 +1,18 @@
 require 'sinatra'
 
+configure do 
+	enable :sessions
+end
+
 before do 
 	content_type :txt
 end
 
-get '/cache' do
- expires 3600, :public, :must_revalidate
- "This page rendered at #{Time.now}."
+get "/set" do 
+	session[:foo] = Time.now
+	"Session value set"
+end
+
+get "/fetch" do 
+	"session value #{session[:foo]}"
 end
